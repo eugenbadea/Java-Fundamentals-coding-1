09c3f82778263a0d4a87b1db74374a2d76d3fb78
EUGEN2037 <badea4@gmail.com>
2022-10-20 19:32:56 +0300

    first commit

---
+++ src/Exercise1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float pi = 3.14f;
+        float perimeter;
+        float diameter=scanner.nextFloat();
+        System.out.println(diameter * 3.14);
+
+    }
+}
