4d3f04a37d43a32c94f2486312b28d3a2b391b37
EUGEN2037 <badea4@gmail.com>
2022-10-21 15:27:10 +0300

    solution 2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Exercise2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float kilogram = scanner.nextFloat();
-        int height = scanner.nextInt();
-          boolean =
-        if("BMI >18.5f || BMI < 24.9") {
+        System.out.println("Introduceti greutatea dumneavoastra in kg:");
+        float weightInKg = scanner.nextFloat();
+        System.out.println("Introduceti inaltimea in cm");
+        int heightInCm = scanner.nextInt();
+        float heightInM = (float) heightInCm / 100;
+        float bmi = weightInKg / (heightInM * heightInM);
+        boolean  bmiIsOptimal = 18.5 < bmi && bmi < 24.9;
+        if(bmiIsOptimal) {
             System.out.println("BMI optimal");
         }else{
-            System.out.println("BMI not optimal");
+            System.out.println("BMI Not optimal");
         }
     }
 }
