b62185d7f24465f087d829084d6c5b61ce784f2a
EUGEN2037 <badea4@gmail.com>
2022-11-01 16:23:53 +0200

    solution 20 a

---
+++ src/ExerciseString.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ExerciseString {
+
+    /*
+    Sa citim un text (o linie de text) de la tastatura
+    Sa afisam inapoi textul insa trebuie sa inlocuim
+      - primul character cu ultimul
+      - al 3 - lea cu al 7 - lea
+      -ultimul cu al 2-lea
+      Exemplu: Ana are mere si pere -> ene are mere si pern
+      Hint: sa folosim metoda .charAt
+     */
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        //Solutie 1
+        // char char 2 = text.charAt(1);
+        // char char7  = text.charAt(6);
+        // char charLast= text.charAt(text.length() - 1;
+        //
+        // String res = charLast + "" + char2 + char7 + text.substring(3, text.length() -1) + char 2;
+        // System.out.println(res);
+
+        char[] chars = text.toCharArray();
+        chars[0] = chars[chars.length - 1];
+        chars[2] = chars[6];
+        chars[chars.length - 1] = chars[1];
+        for (int i = 0; i < chars.length; i++) {
+            System.out.println(chars[i]);
+        }
+
+    }
+}
+
